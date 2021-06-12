32673467de5975ae3fd75a5377853e0a7efb08a7
c-sakshet <c.sakshet@gmail.com>
2021-06-12 19:49:15 +0530

    Initial commits for general enhancements
    1. Added regex.
    2. Added code block and made enhancements in code to support directory recursive search.
    3. Made generic bug fixes.

--- grep.py
+++ grep.py
@@ -1,13 +1,35 @@
-import sys, re
+import sys, re, os
 
+def getListOfFiles(dir_name):
+    '''
+    This function recursively parses all files and directories in the given directory\
+    and returns a list of pathnames to all the files in the directory.
+    '''
+    # paths_of_files_parsed = []
+    ''' 
+    code for traversing to be added
+    '''
+    # return paths_of_files_parsed
+
+    pass
 
 def getUserInput():
-    search_term = sys.argv[1]
-    file_name = sys.argv[2]
-    return search_term, file_name
+    search_term = sys.argv[1] #key word entered by user to search for in the file/s
+    regex = "(" + search_term + ")"
+    keyword = re.compile(regex, flags = re.IGNORECASE)
+
+    list_of_files = []
+    if not(os.path.isdir(sys.argv[2])):
+        if not(os.path.isfile(sys.argv[2])):
+            list_of_files.append(sys.argv[2]) #file entered by user
+        else:
+            return []
+    else:
+        list_of_files = getListOfFiles(sys.argv[2])
+    return keyword, list_of_files
 
 
-def findKeyword(search_term, file_name):
+def findKeyword(search_term, list_of_files):
     '''Function to search a string and return line numbers where it is found in the given file
     - Takes input from the cmd line where first argument is the string to be searched
     and the second is the file to search in
@@ -18,25 +40,52 @@ def findKeyword(search_term, file_name):
     line_number = 0
     found_at_line = []  # Array to store line numbers where the given search string is found
 
-    with open(file_name, 'r') as searchFile:
-        for line_read in searchFile:
-            line_number += 1  # to match the actual line number rather than the index of iterator.
-            if search_term in line_read:
-                found_at_line.append((line_number, line_read.strip('\n'))) # strip new line char at end of line
-                # To Strip right spaces use the following code instead of the above line
-                # found_at_line.append((line_number, line_read.rstrip()))
-    return found_at_line
+    try:
+        for file in list_of_files:
+            with open(file, 'r') as search_file:
+                for line_read in search_file:
+                    line_number += 1  # to match the actual line number rather than the index of iterator.
+                    if search_term.findall(line_read):
+                        found_at_line.append((line_number, line_read.strip('\n'))) # strip new line char at end of line
+                        # To Strip right spaces use the following code instead of the above line
+                        # found_at_line.append((line_number, line_read.rstrip()))
 
+    except:
+        print("Exception", sys.exc_info()[0], "occurred. ")
+        exit()
+    
+    finally:
+        return found_at_line
+        search_file.close()
 
 def main():
     if len(sys.argv) < 3:
-        print("Please follow usage: python3 ./find.py <search keyword> <file name>")
+        print("An unexpected error occurred. Please follow the usage: \n\
+        Linux: python3 ./find.py <search keyword> <file name> \n\
+        Windows: py ./find.py <search keyword> <file name>\n\
+        \
+        \nNote: If searching for special characters like [!,@,#,$,%,^,&] or similar,\
+        \nplease use quotes to denote the string.\n\
+        E.g.: py ./find.py \"python project\" <file name>\
+            ")
+        exit(0)
+
+        '''
+        To Do:
+        Support CLI special character input.
+        '''
+
+    
+    search_term, list_of_files_obtained = getUserInput()
+    if not(search_term):
+        keyword_lines = findKeyword(search_term, list_of_files_obtained)
+    else:
         return
-    search_term, file_name = getUserInput()
-    keyword_lines = findKeyword(search_term, file_name)
+        
     if not keyword_lines:
         print("The search string does not exist in the given file", sys.argv[2])
         return
+        
     for entry in keyword_lines:
         print("Line Number: ", entry[0], ", Line Content: ", entry[1])
         # n = len(sys.argv)
